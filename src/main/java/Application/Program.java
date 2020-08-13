d78bb84a68f8e14be092e00aa263e8cdd1997840
Gustavo Ojeda <gustavovictor23@gmail.com>
2020-08-13 10:02:52 -0400

    Acrescentado o print de boa tarde

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -4,6 +4,7 @@ public class Program {
     public static void main (String [] args){
         
         System.out.println("Bom dia, essa foi uma alteração no código para teste no github");
+        System.out.println("Boa  tarde, essa foi uma alteração no código para teste no github");
         
     }
 }
