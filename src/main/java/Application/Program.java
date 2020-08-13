1bd1655a151593d229ab39d4e07738a867ddbed3
Gustavo Ojeda <gustavovictor23@gmail.com>
2020-08-13 09:47:49 -0400

    Acrescentado print de bom dia

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Application;
 
-/**
- *
- * @author stree
- */
 public class Program {
-    
+    public static void main (String [] args){
+        
+        System.out.println("Bom dia, essa foi uma alteração no código para teste no github");
+        
+    }
 }
